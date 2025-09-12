a5f2a10ccea96c8e420ac809a3dace8c96081ab4
Ullas hm <141505985+Ullas-HM@users.noreply.github.com>
2025-09-12 15:43:56 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import ArtieChatbot from './artie_chatbot.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Artie Chatbot</h1>
+      </header>
+      <main>
+        <ArtieChatbot />
+      </main>
+    </div>
+  );
+}
+
+export default App;
